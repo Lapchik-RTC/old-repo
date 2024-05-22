d9afbd4bbba98bd1ba48a83ba16d7e3be3949035
ivrasst <eho7412@rambler.ru>
2024-05-22 03:19:10 +0300

    начал делать новую угловую передачу

--- software/lapchik_code/hodovaya_lapchik/src/main.cpp
+++ software/lapchik_code/hodovaya_lapchik/src/main.cpp
@@ -1,12 +1,15 @@
 #include <Arduino.h>
-#include "crc8.h"
+/*#include "crc8.h"
 #include "Enotik.h"
 #include "enotik_master.h"
 #include "Enotik_message_format.h"
 #include "motor.h"
 #include "parser.h"
-#include "Periph.h"
-#include "MotorLib.h"
+#include "Periph.h"*/
+#include <mymotor.h>
+//#include "MotorLib.h"
+Mymotor motorLF;
+
 //motor1
 #define ENC1 18
 #define INA1 32
@@ -44,31 +47,31 @@
 #define PWM6 13
 #define CS6 A5
 
-// #define kMotor 2
-// #define kiMotor 0.01
-// //uint8_t motorPins[] = { 8, 9, 5, 4, 10, 11, 7, 6 };
-// uint8_t motorPins[] = { INA1, INB1, INA2, INB2, INA3, INB3, INA4, INB4, INA5, INB5, INA6, INB6 };
-// MOTORS motor[6];
-// int32_t *motorEncs[6];
-// const uint8_t interrupts[] = { ENC1, ENC2, ENC3, ENC4, ENC5, ENC6 };
-// const uint8_t reverses[] = { 1, 0, 1, 0, 1, 0 };
-// const uint8_t directionPins[] = { CS1, CS2, CS3, CS4, CS5, CS6 };
+/*#define kMotor 2
+#define kiMotor 0.01
+//uint8_t motorPins[] = { 8, 9, 5, 4, 10, 11, 7, 6 };
+uint8_t motorPins[] = { INA1, INB1, INA2, INB2, INA3, INB3, INA4, INB4, INA5, INB5, INA6, INB6 };
+MOTORS motor[6];
+int32_t *motorEncs[6];
+const uint8_t interrupts[] = { ENC1, ENC2, ENC3, ENC4, ENC5, ENC6 };
+const uint8_t reverses[] = { 1, 0, 1, 0, 1, 0 };
+const uint8_t directionPins[] = { CS1, CS2, CS3, CS4, CS5, CS6 };
 
-// void interr0() {
-//   *motorEncs[0] = *motorEncs[0] - (reverses[0] * 2 - 1) * (digitalRead(directionPins[0]) * 2 - 1);
-// }
+void interr0() {
+  *motorEncs[0] = *motorEncs[0] - (reverses[0] * 2 - 1) * (digitalRead(directionPins[0]) * 2 - 1);
+}
 
-// void interr1() {
-//   *motorEncs[1] = *motorEncs[1] - (reverses[1] * 2 - 1) * (digitalRead(directionPins[1]) * 2 - 1);
-// }
+void interr1() {
+  *motorEncs[1] = *motorEncs[1] - (reverses[1] * 2 - 1) * (digitalRead(directionPins[1]) * 2 - 1);
+}
 
-// void interr2() {
-//   *motorEncs[2] = *motorEncs[2] - (reverses[2] * 2 - 1) * (digitalRead(directionPins[2]) * 2 - 1);
-// }
+void interr2() {
+  *motorEncs[2] = *motorEncs[2] - (reverses[2] * 2 - 1) * (digitalRead(directionPins[2]) * 2 - 1);
+}
 
-// void interr3() {
-//   *motorEncs[3] = *motorEncs[3] - (reverses[3] * 2 - 1) * (digitalRead(directionPins[3]) * 2 - 1);
-// }
+void interr3() {
+  *motorEncs[3] = *motorEncs[3] - (reverses[3] * 2 - 1) * (digitalRead(directionPins[3]) * 2 - 1);
+}*/
 
 //=================SETUP=================
 void setup() {
@@ -95,5 +98,5 @@ void setup() {
 }
 
 void loop() {
-  Enotik.work();
+  //Enotik.work();
 }
